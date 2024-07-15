efe59ad4a46133bfd6bc7dd674a1da501bac34a8
Mr. WebWizard <142308196+sahilkhan8786@users.noreply.github.com>
2024-07-15 17:36:54 +0530

    Add files via upload

--- src/components/Models/LearningModule.jsx
+++ src/components/Models/LearningModule.jsx
@@ -1,10 +1,15 @@
-import { useGLTF } from '@react-three/drei'
-import React, { useRef } from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react'
 import learningModuleScene from '../../assets/3d/learning_module_scene.glb'
 
 export default function LearningModule({ ...props }) {
     const learningModuleRef = useRef(null)
     const model = useGLTF(learningModuleScene, true)
+    const { actions } = useAnimations(model.animations, learningModuleRef)
+
+    useEffect(() => {
+        actions['mixamo.com_Object_4.001'].play()
+    }, [])
     return (
         <group ref={learningModuleRef} {...props}>
             <primitive object={model.scene} />
